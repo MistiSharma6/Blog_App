a4746591709bb9440e11cadb0f6beb6c1a646671
MistiSharma6 <mistisharma8705@gmail.com>
2026-01-14 19:07:05 +0530

    first commit

---
+++ routes/blog.js
@@ -0,0 +1,85 @@
+const { Router } = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const Blog = require("../models/blog");
+const Comment = require("../models/comment");
+
+const router = Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/images/uploads/`)); //folder
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/add-new", (req, res) => {
+    return res.render("addBlog", {
+        user: req.user,
+        title: "",
+        body: "",
+    });
+});
+
+router.get("/:id", async (req, res) => {
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({ blogId: req.params.id }).populate("createdBy");
+    console.log("blog", blog);
+    return res.render("blog", {
+      user: req.user,
+      blog,
+      comments,
+    });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+    const { content } = req.body;
+
+    if (!content) {
+      return res.redirect(`/blog/${req.params.blogId}?error=Comment is required`);
+    }
+    const comment = await Comment.create({
+      content: req.body.content,
+      blogId: req.params.blogId,
+      createdBy: req.user._id,
+    });
+    return res.redirect(`/blog/${req.params.blogId}`);
+});
+
+router.post("/", upload.single("coverImage"), async (req, res) => {
+    const { title, body } = req.body;
+
+    if (!title || !body) {
+      return res.render("addBlog", {
+        error: "Title and Body are required",
+        user: req.user,
+        title,
+        body,
+      });
+    }
+
+    if (!req.file) {
+      return res.render("addBlog", {
+        error: "Cover Image is required",
+        user: req.user,
+        title,
+        body,
+      });
+    }
+
+    const blog = await Blog.create({
+        title,
+        body,
+        createdBy: req.user._id,
+        coverImageURL: `/uploads/${req.file.filename}`,
+    });
+    return res.redirect(`/blog/${blog._id}`);
+});
+
+module.exports = router;
